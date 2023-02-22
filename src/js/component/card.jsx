26e7d65cb19f0ea847ffb2da3f0c7356eda6e590
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-22 15:46:50 +0000

    updated image

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -8,7 +8,7 @@ export default function MyCard() {
 			<Card style={{ width: "18rem" }}>
 				<Card.Img
 					variant="top"
-					src="https://ichef.bbci.co.uk/news/800/cpsprodpb/51F3/production/_106997902_gettyimages-611696954.jpg.webp"
+					src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdesignshack.net%2Fwp-content%2Fuploads%2Fplaceholder-image.png&f=1&nofb=1&ipt=f6800093ed8a9cc4217d02024c90843992748de4dae672d5e08d2a815e22cc25&ipo=images"
 				/>
 				<Card.Body>
 					<Card.Title>Card Title</Card.Title>
